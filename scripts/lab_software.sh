dcc4e966bceea2c69d00e8bb1d54373777f14438
FlUxIuS <fluxius@gmail.com>
2024-05-18 16:23:30 +0200

    Adding lab software

---
+++ scripts/lab_software.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+function jupiter_soft_install() {
+	goodecho "[+] Installing Jupyter lab"
+	installfromnet "pip3 install jupyterlab"
+	goodecho "[+] Installing Jupyter lab"
+	installfromnet "pip3 install jupyterlab"
+}
