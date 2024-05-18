e7542b541f18c17ab820cd8b636ff64cdc31980a
FlUxIuS <fluxius@gmail.com>
2024-05-18 23:52:25 +0200

    Fixing BATCH file for Windows -> now it's running!

--- build-windows.bat
+++ build-windows.bat
@@ -1,4 +1,4 @@
-@ECHO OFF 
+@ECHO OFF
 
 :: This code is part of RF Switch by @Penthertz
 ::  Author(s): Sébastien Dudek (@FlUxIuS)
@@ -7,17 +7,19 @@ set oldpath=%cd%
 
 TITLE Installing RF Switch for Windows
 
-echo "[+] Compiling RF Switch Go project"
-start "" "C:\Program Files\Go\bin\go.exe build ."
+echo [+] Compiling RF Switch Go project
+cd go/rfswift
+start "" "C:\Program Files\Go\bin\go.exe" "build" "."
 move "rfswift.exe" %oldpath%
+cd %oldpath%
 
+echo [+] Building the Docker images
 set "imagename=myrfswift:latest
 set /p "imagename=Enter image tag value (default: %imagename%): "
 echo %imagename%
 
-set "dockerfile=myrfswift:latest
-set /p "dockerfile=nter value for Dockerfile to use (default: %dockerfile%): "
+set "dockerfile=Dockerfile
+set /p "dockerfile=Enter value for Dockerfile to use (default: %dockerfile%): "
 echo %dockerfile%
 
-
-start "" "docker build . -t %imagename% -f %dockerfile%"
+start "" "docker" "build" "." "-t" "%imagename%" "-f" "%dockerfile%"
