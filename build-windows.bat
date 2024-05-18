e9a929bcb18d9bd418c0910389551e29bc86ab98
FlUxIuS <fluxius@gmail.com>
2024-05-18 23:21:57 +0200

    Fixing bad copies in BAT

--- build-windows.bat
+++ build-windows.bat
@@ -11,23 +11,6 @@ echo "[+] Compiling RF Switch Go project"
 start "" "C:\Program Files\Go\bin\go.exe build ."
 move "rfswift.exe" %oldpath%
 
-
-# Set default values
-DEFAULT_IMAGE="myrfswift:latest"
-DEFAULT_DOCKERFILE="Dockerfile"
-
-# Prompt the user for input with default values
-read -p "Enter image tag value (default: $DEFAULT_IMAGE): " imagename
-read -p "Enter value for Dockerfile to use (default: $DEFAULT_DOCKERFILE): " dockerfile
-
-# Use default values if variables are empty
-imagename=${imagename:-$DEFAULT_IMAGE}
-dockerfile=${dockerfile:-$DEFAULT_DOCKERFILE}
-
-echo "[+] Building the Docker container"
-sudo docker build . -t $imagename -f $dockerfile
-
-
 set "imagename=myrfswift:latest
 set /p "imagename=Enter image tag value (default: %imagename%): "
 echo %imagename%
