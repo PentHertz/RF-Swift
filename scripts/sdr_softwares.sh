a17e26001906b2bfc79365083e79e1217a15802b
FlUxIuS <fluxius@gmail.com>
2024-06-22 01:56:37 +0200

    From source building script for SDR Angel

--- scripts/sdr_softwares.sh
+++ scripts/sdr_softwares.sh
@@ -22,11 +22,137 @@ function sdrangel_soft_install() {
 	goodecho "[+] Downloading and unpacking SDR Angel"
 	[ -d /root/thirdparty ] || mkdir /root/thirdparty
 	cd /root/thirdparty
-	installfromnet "wget https://github.com/f4exb/sdrangel/releases/download/v7.20.0/sdrangel-2563-master.tar.gz"
-	tar xvzf sdrangel-2563-master.tar.gz
-	cd sdrangel-2563-master
-	dpkg -i sdrangel_7.20.0-1_amd64.deb
+	installfromnet "wget https://github.com/f4exb/sdrangel/releases/download/v7.21.3/sdrangel-2700-master.tar.gz"
+	tar xvzf sdrangel-2700-master.tar.gz
+	cd sdrangel-2700-master
+	dpkg -i sdrangel_7.21.3-1_amd64.deb
 	cd /root
+}
+
+function sdrangel_soft_fromsource_install() {
+	goodecho "[+] Installing dependencies"
+	installfromnet "apt-fast update"
+	installfromnet "apt-fast install -y libsndfile-dev git cmake g++ pkg-config autoconf automake libtool libfftw3-dev libusb-1.0-0-dev libusb-dev libhidapi-dev libopengl-dev qtbase5-dev qtchooser libqt5multimedia5-plugins qtmultimedia5-dev libqt5websockets5-dev qttools5-dev qttools5-dev-tools libqt5opengl5-dev libqt5quick5 libqt5charts5-dev qml-module-qtlocation qml-module-qtpositioning qml-module-qtquick-window2 qml-module-qtquick-dialogs qml-module-qtquick-controls qml-module-qtquick-controls2 qml-module-qtquick-layouts libqt5serialport5-dev qtdeclarative5-dev qtpositioning5-dev qtlocation5-dev libqt5texttospeech5-dev qtwebengine5-dev qtbase5-private-dev libqt5gamepad5-dev libqt5svg5-dev libfaad-dev zlib1g-dev libboost-all-dev libasound2-dev pulseaudio libopencv-dev libxml2-dev bison flex ffmpeg libavcodec-dev libavformat-dev libopus-dev doxygen graphviz"
+	goodecho "[+] APT"
+	[ -d /root/thirdparty ] || mkdir /root/thirdparty
+	cd /root/thirdparty
+	installfromnet "git clone https://github.com/srcejon/aptdec.git"
+	cd aptdec
+	git checkout libaptdec
+	git submodule update --init --recursive
+	mkdir build; cd build
+	cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/aptdec ..
+	make -j $(nproc) install
+
+	goodecho "[+] CM265cc"
+	[ -d /root/thirdparty ] || mkdir /root/thirdparty
+	cd /root/thirdparty
+	installfromnet "git clone https://github.com/f4exb/cm256cc.git"
+	cd cm256cc
+	git reset --hard 6f4a51802f5f302577d6d270a9fc0cb7a1ee28ef
+	mkdir build; cd build
+	cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/cm256cc ..
+	make -j $(nproc) install
+
+	goodecho "[+] LibDAB"
+	[ -d /root/thirdparty ] || mkdir /root/thirdparty
+	cd /root/thirdparty
+	installfromnet "git clone https://github.com/srcejon/dab-cmdline"
+	cd dab-cmdline/library
+	git checkout msvc
+	mkdir build; cd build
+	cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/libdab ..
+	make -j $(nproc) install
+
+	goodecho "[+] MBElib"
+	[ -d /root/thirdparty ] || mkdir /root/thirdparty
+	cd /root/thirdparty
+	installfromnet "git clone https://github.com/szechyjs/mbelib.git"
+	cd mbelib
+	git reset --hard 9a04ed5c78176a9965f3d43f7aa1b1f5330e771f
+	mkdir build; cd build
+	cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/mbelib ..
+	make -j $(nproc) install
+
+	goodecho "[+] serialdv"
+	[ -d /root/thirdparty ] || mkdir /root/thirdparty
+	cd /root/thirdparty
+	installfromnet "git clone https://github.com/f4exb/serialDV.git"
+	cd serialDV
+	git reset --hard "v1.1.4"
+	mkdir build; cd build
+	cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/serialdv ..
+	make -j $(nproc) install
+
+	goodecho "[+] DSDcc"
+	[ -d /root/thirdparty ] || mkdir /root/thirdparty
+	cd /root/thirdparty
+	installfromnet "git clone https://github.com/f4exb/dsdcc.git"
+	cd dsdcc
+	git reset --hard "v1.9.5"
+	mkdir build; cd build
+	cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/dsdcc -DUSE_MBELIB=ON -DLIBMBE_INCLUDE_DIR=/opt/install/mbelib/include -DLIBMBE_LIBRARY=/opt/install/mbelib/lib/libmbe.so -DLIBSERIALDV_INCLUDE_DIR=/opt/install/serialdv/include/serialdv -DLIBSERIALDV_LIBRARY=/opt/install/serialdv/lib/libserialdv.so ..
+	make -j $(nproc) install
+
+	goodecho "[+] Codec2"
+	[ -d /root/thirdparty ] || mkdir /root/thirdparty
+	cd /root/thirdparty
+	installfromnet "apt-fast -y install libspeexdsp-dev libsamplerate0-dev"
+	git clone https://github.com/drowe67/codec2-dev.git codec2
+	cd codec2
+	git reset --hard "v1.0.3"
+	mkdir build_linux; cd build_linux
+	cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/codec2 ..
+	make -j $(nproc) install
+
+	goodecho "[+] SGP4"
+	[ -d /root/thirdparty ] || mkdir /root/thirdparty
+	cd /root/thirdparty
+	installfromnet "git clone https://github.com/dnwrnr/sgp4.git"
+	cd sgp4
+	mkdir build; cd build
+	cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/sgp4 ..
+	make -j $(nproc) install
+
+	goodecho "[+] libsigmf"
+	[ -d /root/thirdparty ] || mkdir /root/thirdparty
+	cd /root/thirdparty
+	installfromnet "git clone https://github.com/f4exb/libsigmf.git"
+	cd libsigmf
+	git checkout "new-namespaces"
+	mkdir build; cd build
+	cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/libsigmf .. 
+	make -j $(nproc) install
+
+	goodecho "[+] ggmorse"
+	[ -d /root/thirdparty ] || mkdir /root/thirdparty
+	cd /root/thirdparty
+	installfromnet "git clone https://github.com/ggerganov/ggmorse.git"
+	cd ggmorse
+	mkdir build; cd build
+	cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/opt/install/ggmorse -DGGMORSE_BUILD_TESTS=OFF -DGGMORSE_BUILD_EXAMPLES=OFF ..
+	make -j $(nproc) install
+
+	goodecho "[+] Installing SDR Angel"
+	[ -d /root/thirdparty ] || mkdir /root/thirdparty
+	cd /root/thirdparty
+	installfromnet "git clone https://github.com/f4exb/sdrangel.git"
+	cd sdrangel
+	mkdir build; cd build
+	cmake -Wno-dev -DDEBUG_OUTPUT=ON -DRX_SAMPLE_24BIT=ON \
+	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+	-DAPT_DIR=/opt/install/aptdec \
+	-DCM256CC_DIR=/opt/install/cm256cc \
+	-DDSDCC_DIR=/opt/install/dsdcc \
+	-DSERIALDV_DIR=/opt/install/serialdv \
+	-DMBE_DIR=/opt/install/mbelib \
+	-DCODEC2_DIR=/opt/install/codec2 \
+	-DSGP4_DIR=/opt/install/sgp4 \
+	-DLIBSIGMF_DIR=/opt/install/libsigmf \
+	-DDAB_DIR=/opt/install/libdab \
+	-DGGMORSE_DIR=/opt/install/ggmorse \
+	-DCMAKE_INSTALL_PREFIX=/opt/install/sdrangel ..
+	make -j $(nproc) install
 }
 
 function sdrpp_soft_fromsource_install () { # Beta test, but should work on almost all platforms
