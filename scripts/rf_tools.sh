713be65ca19959173550d41309b63a5af06a1a5b
FlUxIuS <fluxius@gmail.com>
2024-07-17 15:17:43 +0200

    Fixing Kismet installation

--- scripts/rf_tools.sh
+++ scripts/rf_tools.sh
@@ -4,7 +4,7 @@
 
 function kismet_soft_install() {
 	goodecho "[+] Installing Kismet"
-	installfromnet "wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg >/dev/null"
+	wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg >/dev/null
 	echo 'deb [signed-by=/usr/share/keyrings/kismet-archive-keyring.gpg] https://www.kismetwireless.net/repos/apt/release/jammy jammy main' | sudo tee /etc/apt/sources.list.d/kismet.list >/dev/null
 	installfromnet "apt-fast update"
 	installfromnet "apt-fast -y install kismet"
