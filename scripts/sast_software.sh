4bee18d9c4dfe06ec42c39435733e1317f4935d6
djnn <email@djnn.sh>
2024-07-21 12:20:03 +0200

    fix(sast): using canonical function for build
    also changed the ghidra version to a more updated one

--- scripts/sast_software.sh
+++ scripts/sast_software.sh
@@ -7,58 +7,53 @@ set -euo pipefail
 
 
 function LLVM_install() { # expects llvm version
-    echo "[+] installing LLVM 17"
+    LLVM_VERSION=17
 
-    wget -c https://apt.llvm.org/llvm.sh && chmod +x llvm.sh
-    sudo ./llvm.sh 17
+    goodecho "[+] installing LLVM ${LLVM_VERSION}"
 
-    export LLVM_CONFIG=llvm-config-${1}
+    [ -d /root/thirdparty ] || mkdir -p /root/thirdparty
+    cd /root/thirdparty
+
+    installfromnet "wget -c https://apt.llvm.org/llvm.sh"
+
+    chmod +x llvm.sh
+    ./llvm.sh ${LLVM_VERSION}
+
+    export LLVM_CONFIG=llvm-config-${LLVM_VERSION}
     echo "Defaults env_keep += \"${LLVM_CONFIG}\"" | sudo EDITOR='tee -a' visudo
 }
 
+function semgrep_install() {
+    goodecho "[+] installing AFL deps"
+    installfromnet "pip3 install semgrep"
+}
+
+function cppcheck_install() {
+    goodecho "[+] installing cppcheck"
+    installfromnet "apt-fast install -y cppcheck"
+}
+
 function AFL_install() {
-    echo "[+] installing AFL deps"
+    goodecho "[+] installing AFL++"
 
-    sudo apt-get install -y gcc
-
-    sudo apt-get install -y build-essential make python3-dev automake cmake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools python3-pip \
-        gcc-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-plugin-dev libstdc++-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-dev \
-        ninja-build pipx binutils-dev cppcheck
-
-    pipx ensurepath
-
-    pip install unicorn
-    pipx install semgrep
-    echo "[+] installing libAFL"
-
-    curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf > install-rust.sh
-    chmod +x install-rust.sh
-
-    ./install-rust.sh -y
-
-    . "$HOME/.cargo/env"
-
-    cargo install cargo-make
-    git clone https://github.com/AFLplusplus/LibAFL libafl --branch=main
-    cd libafl ; cargo build --release ; cd ..
-
-     echo "[+] installing AFL++"
-
-    git clone https://github.com/AFLplusplus/AFLplusplus aflpp --branch=stable
-    cd aflpp ; make && sudo make install ; cd -
+    installfromnet "apt-fast install -y clang build-essential afl++"
 }
 
 function honggfuzz_install() {
     echo "[+] installing honggfuzz"
 
-    sudo apt-get -y  install binutils-dev libunwind-dev libblocksruntime-dev
+    installfromnet "apt-fast install -y binutils-dev libunwind-dev libblocksruntime-dev git"
+
+    [ -d /root/thirdparty ] || mkdir -p /root/thirdparty
+    cd /root/thirdparty
+
     git clone https://github.com/google/honggfuzz
 
-    cd honggfuzz ; make ; cd ..
+    cd honggfuzz && make && make install
 }
 
 function clang_static_analyzer_install() {
     echo "[+] installing clang-static-analyzer"
 
-    sudo apt-get install -y clang clang-tools
+    installfromnet "apt-fast install -y clang clang-tools"
 }
