d223b9e03836dc5cebe5a317f8b8f21da242a524
FlUxIuS <fluxius@gmail.com>
2024-12-05 12:57:26 +0100

    Incrementing version

--- go/rfswift/common/common.go
+++ go/rfswift/common/common.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-var Version = "0.5.2"
+var Version = "0.5.3"
 var Branch = "main"
 var ascii_art = `                                                                                                    
                                                                                                                                                       
